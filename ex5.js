4e76da5dde3cd69f9b6a46d09ad153b26de6d50c
danacalcaianu <danacalcaianu@yahoo.com>
2017-09-29 11:05:27 +0300

    Update ex5.js

--- ex5.js
+++ ex5.js
@@ -1,15 +1,23 @@
 //Lexical scope
+var age = 22;
 
 function grandfather() {
-    var name = 'Some name';
-    // likes is not accessible here
+    var name = 'grandfather name';
+    var likes = "dancing";
     function parent() {
-        // name is accessible here
-        // likes is not accessible here
+         name = "parent name";
+         age = 35;
         function child() {
-             // name is also accessible here
             // Innermost level of the scope chain
+            name = "child name"
             var likes = 'Coding';
         }
+        child();
     }
+    parent();
+    console.log(name);
+    console.log(likes);
+    console.log(age);
 }
+
+grandfather();
